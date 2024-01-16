b7b7633b4ee081acc99b230faf26d9298339d507
unknown <as4044579@gmail.com>
2024-01-16 19:04:19 +0530

    creating specialize section

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,116 +1,151 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaRegEnvelope } from "react-icons/fa6";
+//import { CiLocationOn } from "react-icons/ci";
+import { FaPhone } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FOOTER_LOGO } from "./Constant";
 import "./style.scss";
-import { WLOGO } from "./Constant";
-
-import { TfiLocationPin } from "react-icons/tfi";
-import { BsEnvelope, BsTelephoneInbound } from "react-icons/bs";
-import { GrLinkedin } from "react-icons/gr";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { FiYoutube } from "react-icons/fi";
 
 const Footer = () => {
   return (
     <>
-      <section></section>
-      <footer className="main_footer ">
-        <div className="container">
+      {/* <section className="container">
+        <div className="row">
+          <div className="col-md-4">dfcgvh</div>
+          <div className="col-md-8">dfcgvh</div>
+        </div>
+      </section> */}
+
+      <footer className="footer_section">
+        <div className="container ftr_top">
           <div className="row">
-            <div className="col-md-3 mt-4">
-              <div className="logo">
-                <img src={WLOGO} alt="LOGO" />
-              </div>
-              <br />
-              <h2 className="text-decoration-underline ">
-                Don't Hesite to contact us
-              </h2>
-              <ul className=" footer_icons list-unstyled text-blackfont-medium ">
-                <li className="mt-4 ">
-                  <BsEnvelope />
-                  &nbsp;{" "}
-                  <Link
-                    to="mailto:sakshath@gmail.com"
-                    className="social text-white text-decoration-none"
-                  >
-                    sakshath-technologies@gmail.com
+            {/*  */}
+            <div className="col-md-3 ftr_col1">
+              <img className="pb-4" src={FOOTER_LOGO} alt="" />
+              <h4 className="text-decoration-underline mt-md-3">
+                Don’t Hesitate to Conatct With Our Experites
+              </h4>
+              <ul className=" font-medium ">
+                <li className="contact_hover">
+                  <Link to="mailto:sakshath-technologies@gmail.com">
+                    <FaRegEnvelope />
+                    &nbsp; sakshath-technologies@gmail.com
                   </Link>
                 </li>
-                <li className="mt-4">
-                  <TfiLocationPin /> &nbsp;
-                  <Link
-                    to="/"
-                    className=" text-decoration-none font-size-medium"
-                  >
-                    Jakkur Main Road
-                  </Link>
+                <li className="mt-3 ftr_addr ">
+                  <p>
+                    8, KV Jairam Rd, MCHS Layout, Jakkur,Bengaluru – 560 064
+                  </p>
                 </li>
-                <li className="mt-4">
-                  <BsTelephoneInbound /> &nbsp;
-                  <Link
-                    to="tel:+91 897979780"
-                    className=" text-decoration-none font-size-larger"
-                  >
-                    8977978780
+                <li className="mt-3 contact_hover">
+                  <Link to="tel:+91 80 67819878">
+                    <FaPhone /> +91 80 67819878
                   </Link>
                 </li>
               </ul>
-
-              {/* </div> */}
             </div>
-
-            <div className="col-md-3  mt-4 footer_links">
-              <h3 className="text-decoration-underline">Quick Links</h3>
-              <ul className="text-black ">
+            <div className="col-md-3 footer_link ">
+              <h5>Best Services</h5>
+              <ul className="">
                 <li className="mt-4">
-                  <Link to="/">Home </Link>{" "}
+                  <Link to="/">Web & IT Consulting</Link>
                 </li>
-                <li className="mt-3 justify-content-center ">
-                  {" "}
-                  <Link to="/">About-Us</Link>
+                <li className="mt-3">
+                  <Link to="/">Cyber Security Solutions</Link>
                 </li>
-                <li className="mt-4 ">
-                  <Link to="/">Service</Link>
+                <li className="mt-3">
+                  <Link to="/about">Software Development</Link>
                 </li>
-                <li className="mt-4 ">
-                  <Link to="/">Blogs</Link>
+                <li className="mt-3">
+                  <Link to="/about">Product Enginering</Link>
                 </li>
-                <li className="mt-4 ">
-                  <Link to="/">Careers</Link>
+                <li className="mt-3">
+                  <Link to="/about">Project Management</Link>
                 </li>
-                <li className="mt-4 ">
-                  <Link to="/">Banking Solution</Link>
+                <li className="mt-3">
+                  <Link to="/about">Security Solutions</Link>
                 </li>
               </ul>
             </div>
-
-            <div className="col-md-3  mt-4">
-              <p>ggger</p>
+            <div className="col-md-3 footer_link">
+              <h5>IT Company</h5>
+              <ul>
+                <li className="mt-4">
+                  <Link to="">About Company</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Latest News & Blog</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Professional Members</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Our Achievment</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Company Journey</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Meet Out Teams</Link>
+                </li>
+              </ul>
             </div>
-            <div className="col-md-3  mt-4">
-              <p>ggger</p>
+            <div className="col-md-3 footer_link">
+              <h5>Our Support</h5>
+
+              <ul className="font-medium">
+                <li className="mt-4">
+                  <Link to="">Premium Support</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Need a Career ?</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Help & FAQ</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Pricing and plans</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Cookies Policy</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Privacy Policy</Link>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="container">
-          <hr />
-        </div>
 
-        <div className="container footer_rights">
-          <div className="row">
-            <div className="col-md-6">
-              Copyright © 2024 Sakshath-technologies. All Right Reserved
+        <div className="container ftr_bottom">
+          <div className="row ">
+            <div className="col-md-8">
+              <div>
+                <p>
+                  Copyrights © 2023 Sakshath Technologies®. All rights reserved.
+                </p>
+              </div>
             </div>
-            <div className="col-md-6 footer_socail_link ">
+
+            <div className="col-md-4 social_media ">
               <Link to="">
-                <GrLinkedin />
+                <FaFacebookF />
               </Link>
 
               <Link to="">
-                <AiOutlineInstagram />
+                <FaTwitter />
               </Link>
 
               <Link to="">
-                <FiYoutube />
+                <FaLinkedin />
+              </Link>
+
+              <Link to="">
+                <FaYoutube />
               </Link>
             </div>
           </div>
