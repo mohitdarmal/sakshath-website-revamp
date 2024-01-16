1e16af82195f34ed24f123754cbdd68f02479930
unknown <nandinichourey08@gmail.com>
2024-01-16 15:52:54 +0530

    working on footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,10 +1,163 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
+import { FaPhone } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FOOTER_LOGO } from "./constant";
+import "./style.scss";
 
 const Footer = () => {
   return (
     <>
-      <div>index Footer</div>
-      <p>Copy Right || All rights reserved by Mohit Darmal</p>
+      {/* <section className="container">
+        <div className="row">
+          <div className="col-md-4">dfcgvh</div>
+          <div className="col-md-8">dfcgvh</div>
+        </div>
+      </section> */}
+
+      <footer className="footer_section">
+        <div className="container ">
+          <div className="row">
+          {/*  */}
+            <div className="col-md-3 mt-3">
+              <img src={FOOTER_LOGO} alt="" />
+              <h4 className="text-decoration-underline font_color">
+                Don’t Hesitate to Conatct With Our Experites
+              </h4>
+              <ul className=" font-medium list_type align_items">
+                <li className="mt-4 contact_hover">
+                  <Link to="mailto:sakshath-technologies@gmail.com">
+                    <FaRegEnvelope /> sakshath-technologies@gmail.com
+                  </Link>
+                </li>
+                <li className="mt-3 ">
+                  <p className="footer_section">
+                    <CiLocationOn />  8, KV Jairam Rd, MCHS Layout, Jakkur,Bengaluru – 560 064
+                  </p>
+                </li>
+                <li className="mt-3 contact_hover">
+                  <Link to="tel:+91 80 67819878">
+                    <FaPhone /> +91 80 67819878
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 footer_link mt-4">
+              <h2 className="text-decoration-underline font_color mx-2">
+                Best Services
+              </h2>
+              <ul className=" font_color font-medium ">
+                <li className="mt-4">
+                  <Link to="/">Web & IT Consulting</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/">Cyber Security Solutions</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Software Development</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Product Enginering</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Project Management</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Security Solutions</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 footer_link mt-4">
+              <h2 className="text-decoration-underline font_color mx-2">
+                IT Company
+              </h2>
+              <ul className="font_color font-medium">
+                <li className="mt-4">
+                  <Link to="">About Company</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Latest News & Blog</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Professional Members</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Our Achievment</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Company Journey</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Meet Out Teams</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 footer_link mt-4">
+              <h2 className="text-decoration-underline font_color mx-2">
+                Our Support
+              </h2>
+
+              <ul className="font_color font-medium">
+                <li className="mt-4">
+                  <Link to="">Premium Support</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Need a Career ?</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Help & FAQ</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Pricing and plans</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Cookies Policy</Link>
+                </li>
+                <li className="mt-3">
+                  <Link to="/about">Privacy Policy</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <hr />
+        </div>
+        <div className="container">
+          <div className="row ">
+            <div className="col-md-8">
+              <div className="font_color">
+                <p>
+                  Copyrights © 2023 Sakshath Technologies®. All rights reserved.
+                </p>
+              </div>
+            </div>
+
+            <div className="col-md-4 social_media ">
+              <Link to="">
+                <FaFacebookF />
+              </Link>
+
+              <Link to="">
+                <FaTwitter />
+              </Link>
+
+              <Link to="">
+                <FaLinkedin />
+              </Link>
+
+              <Link to="">
+                <FaYoutube />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='my-5 py-5'>
+      Home
+    </div>
   )
 }
 
