cd4c49183ba5ba1f5c1bde7996a2e5798c0043a8
unknown <nandinichourey08@gmail.com>
2024-01-22 18:54:56 +0530

    service page created

---
+++ src/Pages/Service/Constant.js
@@ -0,0 +1,4 @@
+export const SERVICE_HAPPY_CUSTOMER = require("../../Assets/images/service_happy_customer.jpg");
+export const IT_BENEFITS_LEFT_TOP_IMG = require("../../Assets/images/IT_benefits_img_left_top.jpg");
+export const IT_BENEFITS_LEFT_BOTTOM_IMG = require("../../Assets/images/IT_benefits_left_bottom_img.jpg");
+export const IT_BENEFITS_RIGHT_IMG = require("../../Assets/images/IT_benefits_right_img.jpg")
