355ee3150d0e58b72d33a00c70b070ae87fae2d8
unknown <nandinichourey08@gmail.com>
2024-01-22 10:10:05 +0530

    Faq section created

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -251,7 +251,7 @@ const Footer = () => {
                       Duis aute irure dolor in reprehenderit in voluptate velit
                       esse cillum dolore eu fugiat nulla pariatur. Excepteur
                       sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum.
+                      officia deserunt mollit anim id est laborum. 
                     </Accordion.Body>
                   </Accordion.Item>
                 </Accordion>
