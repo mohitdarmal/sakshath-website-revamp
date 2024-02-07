f375c8213beae45c58e6dcd7bc07f66e3c5efb53
unknown <nandinichourey08@gmail.com>
2024-02-07 20:30:17 +0530

    working on career page

--- src/Pages/Careers/Constant.js
+++ src/Pages/Careers/Constant.js
@@ -1,4 +1,5 @@
 // export const CARRER_IMG= require('../../Assets/images/career_img.png');
 export const CARRER_BENFIT_IMG=require("../../Assets/images/about-1.jpg");
 export const JOIN_OUR_TEAM_IMG = require("../../Assets/images/join_our_team.png");
-export const CAREERS_AT_SAKSHATH = require("../../Assets/images/careers_at_sakshath.png")
+export const CAREERS_AT_SAKSHATH = require("../../Assets/images/careers_at_sakshath.png");
+export const REACT_LOGO = require("../../Assets/images/physics.png")
