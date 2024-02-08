232fa7cf3107a19b13b743687a68105c46635cef
unknown <as4044579@gmail.com>
2024-02-08 17:58:15 +0530

    working on Automation

---
+++ src/Components/Graph/index.js
@@ -0,0 +1,159 @@
+// import React from "react";
+// import ReactApexChart from "react-apexcharts";
+// import Chart from "react-apexcharts";
+
+// class PieChart extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       series: [25, 15, 44, 55, 41],
+//       options: {
+//         chart: {
+//           width: "100%",
+//           type: "pie",
+//         },
+//         labels: [
+//           "Process Discovery ",
+//           "Process Analysis",
+//           "Process Monitoring",
+//           "Process Redesign",
+//           "Process Implementation",
+//         ],
+//         theme: {
+//           monochrome: {
+//             enabled: true,
+//           },
+//         },
+//         plotOptions: {
+//           pie: {
+//             dataLabels: {
+//               offset: -5,
+//             },
+//           },
+//         },
+//         title: {
+//           // text: "Monochrome Pie"
+//         },
+//         dataLabels: {
+//           formatter(val, opts) {
+//             const name = opts.w.globals.labels[opts.seriesIndex];
+//             return [name, val.toFixed(1) + "%"];
+//           },
+//         },
+//         legend: {
+//           show: false,
+//         },
+//       },
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div id="chart">
+//           {/* <ReactApexChart options={this.state.options} series={this.state.series} type="pie" /> */}
+//         </div>
+//         <div id="html-dist">
+//           <div className="pie">
+//             <Chart
+//               options={this.state.options}
+//               series={this.state.series}
+//               type="pie"
+//               width="540"
+//               height="540"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default PieChart;
+
+
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import Chart from "react-apexcharts";
+
+class PieChart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    series: [20,20,20,20,20],
+      // Removed Process Implementation
+      options: {
+        chart: {
+          width: "100%",
+          type: "pie",
+        },
+        labels: [
+          "Process Discovery ",
+          "Process Analysis",
+          "Process Monitoring",
+          "Process Redesign",
+          "Process Monitoring"
+        ], // Adjusted labels accordingly
+        theme: {
+          monochrome: {
+            enabled: true, // Disabled monochrome theme for colors
+          },
+        },
+        colors: [
+          "#ff4500ab", // Red
+          "#808080c9", // grey
+          "#df960fc9",  //ornage
+         
+          "#32a4d5", // Blue
+          "#13aa13ad", // Green
+        ], // Added colors
+        // plotOptions: {
+        //   pie: {
+        //     dataLabels: {
+        //       offset: -5,
+        //     },
+        //   },
+        // },
+        title: {
+          // text: "Monochrome Pie"
+        },
+        dataLabels: {
+          formatter(val, opts) {
+            const name = opts.w.globals.labels[opts.seriesIndex];
+            // return [name, val.toFixed(1) + "%"];
+             return [name, val.toFixed(1)+ ""];
+          },
+        },
+        legend: {
+          show: false,
+        },
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="chart">
+          {/* <ReactApexChart options={this.state.options} series={this.state.series} type="pie" /> */}
+        </div>
+        <div id="html-dist">
+          <div className="pie">
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="pie"
+              width="550"
+              height="540"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PieChart;
--- src/Pages/Service/Automation/Constant.js
+++ src/Pages/Service/Automation/Constant.js
@@ -1,2 +1,14 @@
 export const AUTOMATION_BG_IMG=require("../../../Assets/images/Automation_bg_img1.png");
-export const CHECK_ICON=require('../../../Assets/images/check-mark (2).png')
+export const CHECK_ICON=require('../../../Assets/images/check-mark (2).png');
+
+
+// mobile_development
+
+export const ANDRIOD_APP=require('../../../Assets/images/Andriod_App.svg').default;
+export const IOS_APPLE=require('../../../Assets/images/ios_app_aplple.svg').default;
+export const CROSS_PLATFORM_APP=require('../../../Assets/images/Cross Platform App Development.svg').default;
+
+
+
+// Application Integration
+
