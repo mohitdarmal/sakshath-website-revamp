166e1397b325f06c8d766e43f5485390a288bbd4
Mohit Darmal <mohitdarmal1907@gmail.com>
2024-02-15 19:16:34 +0530

    working on contacgt us page

--- src/Pages/Contactus/Constant.js
+++ src/Pages/Contactus/Constant.js
@@ -1,3 +1,7 @@
 export const LINKEDIN_ICON = require("../../Assets/images/linkedin_icon.png");
 export const INSTAGRAM_ICON = require("../../Assets/images/instagram_icon.png");
 export const YOUTUBE_ICON = require("../../Assets/images/youtube_icon.png");
+
+
+export const HEADQUATERS_ICON = require("../../Assets/images/headquaters_img.png");
+export const REGISTERED_ICON = require("../../Assets/images/registered_office.png");
--- src/Pages/Contactus/index.js
+++ src/Pages/Contactus/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState } from 'react';
+import {Tab, Tabs, Col, Nav, Row} from 'react-bootstrap';
 import "./style.scss"
 import { GrLocation } from "react-icons/gr";
 //import SlideAnimationButton from '../../Components/Utils/SlideAnimationButton';
@@ -6,33 +7,134 @@ import {Link} from "react-router-dom";
 import { BsTelephoneInbound } from "react-icons/bs";
 import { FaRegEnvelope } from "react-icons/fa6";
 
-import { INSTAGRAM_ICON, LINKEDIN_ICON, YOUTUBE_ICON } from "./Constant";
+import { INSTAGRAM_ICON, LINKEDIN_ICON, YOUTUBE_ICON, HEADQUATERS_ICON, REGISTERED_ICON } from "./Constant";
 import Contact from "../../Components/Contact/Contact";
 import BreadCumb from "../../Components/Breadcumb";
 import { ABOUT_BANNER } from '../../Components/Breadcumb/Constant';
+import { FaPaperPlane } from "react-icons/fa";
+import Heading from '../../Components/Utils/Heading';
  
-import MapComponent from '../../Components/GoogleMap/index';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 const Contactus = () => {
-  const defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
+  const [key, setKey] = useState('home');
   return (
     <>
     <BreadCumb
         imgUrl={ABOUT_BANNER}
         title="Contact Us"
       />
-        <section  className='contact_us'>
 
-     
-    
-    
+<section className='contact_us_sec'>
+    <div className='container'>
+      <div className='row'>
+      <div className='col-md-1'> </div>
+        <div className='col-md-5 position-relative'>
+           <div className='head_location'>
+              <img src={HEADQUATERS_ICON} />
+              <h2>HeadQuater Office</h2>
+              <ul>
+                <li className='address'>Address: 8, KV Jairam Rd, MCHS Layout, Jakkuru Layout, Jakkur, Bengaluru, Karnataka 560064</li>
+                <li className='email'><Link to="mailto:info@sakshath-technologies.com">info@sakshath-technologies.com</Link></li>
+                <li className='phn'><Link to="tel:+918067819878">+91 8067819878</Link></li>
+              </ul>
+              <Link className='get_direction' to="" target="_blank"><FaPaperPlane /> Get Direction</Link>
+           </div>
+        </div>
+
+        <div className='col-md-5 position-relative'>
+        <div className='head_location'>
+              <img src={REGISTERED_ICON} />
+              <h2>Registered Office</h2>
+              <ul>
+                <li className='address'>Address: 8, KV Jairam Rd, MCHS Layout, Jakkuru Layout, Jakkur, Bengaluru, Karnataka 560064</li>
+                <li className='email'><Link to="mailto:info@sakshath-technologies.com">info@sakshath-technologies.com</Link></li>
+                <li className='phn'><Link to="tel:+918067819878">+91 8067819878</Link></li>
+              </ul>
+              <Link className='get_direction' to="" target="_blank"><FaPaperPlane /> Get Direction</Link>
+           </div>
+        </div>
+        <div className='col-md-1'> </div>
+      </div>
+
+<div className='row justify-content-center contact_social_icon'>
+<img src={INSTAGRAM_ICON} /> <img src={LINKEDIN_ICON} />
+</div>
+
+    </div>
+</section>
+
+
+
+{/* <section> 
+  <div className='container'>
+    <div className='row'>
+
+    </div>
+  </div>
+</section> */}
+<section className='our_location_sec'>
+  <div className="container">
+
+<div className='row'>
+  <div className='text-center'>
+  <Heading heading="Our Location" uppercase="text-uppercase" />
+  </div>
+</div>
+
+    <div className="row our_location_tab">
+      <div className='col-md-12'>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+      {/* Asia Start */}
+      <Tab eventKey="home" title="Asia">
+          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Row>
+            <Col sm={3}>
+              <Nav variant="pills" className="flex-column">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">First tab content</Tab.Pane>
+                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </Tab>
+      {/* Asia End */}
+      
+      <Tab eventKey="us" title="US">
+        US
+      </Tab>
+      <Tab eventKey="anz" title="ANZ">
+        ANZ
+      </Tab>
+      <Tab eventKey="aftrica" title="Africa">
+        Africa
+      </Tab>
+      <Tab eventKey="middleeast" title="Middle East">
+        Middle East
+      </Tab>
+      <Tab eventKey="euorpe" title="Europe">
+        Europe
+      </Tab>
+    </Tabs>
+      </div>
+    </div>
+  </div>
+</section>
+
+        {/* <section  className='contact_us'>
 
          <div className="container contact_us_box">
           <div className="row">
@@ -131,7 +233,7 @@ Gurugram, Haryana 122016
                   <h3>Follow us on</h3>
                   <div className="contactus_social_media">
                   <Link to="">
-                {/* <RiInstagramFill /> */}<img src={LINKEDIN_ICON} />
+                 <img src={LINKEDIN_ICON} />
               </Link>
 
               <Link to="">
@@ -163,28 +265,24 @@ Gurugram, Haryana 122016
         <section className='contact_us_map_form_section'>
         <div className="container mt-4">
           <div className="row">
-            {/* <span>Contact Us</span> */}
+    
             <div className="col-md-6">
               <iframe
                 src="https://www.google.com/maps/d/embed?mid=1QtXoNJa4bNr3llNKRLddH_rzCUNktWQ&ehbc=000F&noprof=1"
                 width="100%"
                 height="545"
               ></iframe>
-              {/* <iframe
-    src="https://www.google.com/maps/d/embed?mid=1QtXoNJa4bNr3llNKRLddH_rzCUNktWQ"
-    width="100%"
-    height="480"
-></iframe> */}
+
             </div>
 
             <div className="col-md-6">
-              {/* contact */}
+            
               <Contact />
-              {/* contact ends */}
+              
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
         
     </>
   )
