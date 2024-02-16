7c0d1a50a054a82728321a3f100dac1dfcfed2ac
mohitdarmal <mohitdarmal1996@gmail.com>
2024-02-16 10:25:47 +0530

    working on contact us page

--- src/Pages/Contactus/index.js
+++ src/Pages/Contactus/index.js
@@ -1,140 +1,194 @@
-import { useState } from 'react';
-import {Tab, Tabs, Col, Nav, Row} from 'react-bootstrap';
-import "./style.scss"
+import { useState } from "react";
+import { Tab, Tabs, Col, Nav, Row } from "react-bootstrap";
+import "./style.scss";
 import { GrLocation } from "react-icons/gr";
 //import SlideAnimationButton from '../../Components/Utils/SlideAnimationButton';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsTelephoneInbound } from "react-icons/bs";
 import { FaRegEnvelope } from "react-icons/fa6";
 
-import { INSTAGRAM_ICON, LINKEDIN_ICON, YOUTUBE_ICON, HEADQUATERS_ICON, REGISTERED_ICON } from "./Constant";
+import {
+  HEADQUATERS_ICON,
+  REGISTERED_ICON,
+  YOUTUBE_ICON,
+  INSTAGRAM_ICON,
+  LINKEDIN_ICON
+} from "./Constant";
 import Contact from "../../Components/Contact/Contact";
 import BreadCumb from "../../Components/Breadcumb";
-import { ABOUT_BANNER } from '../../Components/Breadcumb/Constant';
+import { ABOUT_BANNER } from "../../Components/Breadcumb/Constant";
 import { FaPaperPlane } from "react-icons/fa";
-import Heading from '../../Components/Utils/Heading';
- 
+import Heading from "../../Components/Utils/Heading";
+
 const Contactus = () => {
-  const [key, setKey] = useState('home');
+  const [key, setKey] = useState("home");
   return (
     <>
-    <BreadCumb
-        imgUrl={ABOUT_BANNER}
-        title="Contact Us"
-      />
+      <BreadCumb imgUrl={ABOUT_BANNER} title="Contact Us" />
 
-<section className='contact_us_sec'>
-    <div className='container'>
-      <div className='row'>
-      <div className='col-md-1'> </div>
-        <div className='col-md-5 position-relative'>
-           <div className='head_location'>
-              <img src={HEADQUATERS_ICON} />
-              <h2>HeadQuater Office</h2>
-              <ul>
-                <li className='address'>Address: 8, KV Jairam Rd, MCHS Layout, Jakkuru Layout, Jakkur, Bengaluru, Karnataka 560064</li>
-                <li className='email'><Link to="mailto:info@sakshath-technologies.com">info@sakshath-technologies.com</Link></li>
-                <li className='phn'><Link to="tel:+918067819878">+91 8067819878</Link></li>
-              </ul>
-              <Link className='get_direction' to="" target="_blank"><FaPaperPlane /> Get Direction</Link>
-           </div>
-        </div>
+      <section className="contact_us_sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-1"> </div>
+            <div className="col-md-5 position-relative">
+              <div className="head_location">
+                <img src={HEADQUATERS_ICON} />
+                <h2>HeadQuater Office</h2>
+                <ul>
+                  <li className="address">
+                    Address: 8, KV Jairam Rd, MCHS Layout, Jakkuru Layout,
+                    Jakkur, Bengaluru, Karnataka 560064
+                  </li>
+                  <li className="email">
+                    <Link to="mailto:info@sakshath-technologies.com">
+                      info@sakshath-technologies.com
+                    </Link>
+                  </li>
+                  <li className="phn">
+                    <Link to="tel:+918067819878">+91 8067819878</Link>
+                  </li>
+                </ul>
+                <Link
+                  className="get_direction"
+                  to="https://maps.app.goo.gl/UqW4zSNRHr8YN18D7"
+                  target="_blank"
+                >
+                  <FaPaperPlane /> Get Direction
+                </Link>
+              </div>
+            </div>
 
-        <div className='col-md-5 position-relative'>
-        <div className='head_location'>
-              <img src={REGISTERED_ICON} />
-              <h2>Registered Office</h2>
-              <ul>
-                <li className='address'>Address: 8, KV Jairam Rd, MCHS Layout, Jakkuru Layout, Jakkur, Bengaluru, Karnataka 560064</li>
-                <li className='email'><Link to="mailto:info@sakshath-technologies.com">info@sakshath-technologies.com</Link></li>
-                <li className='phn'><Link to="tel:+918067819878">+91 8067819878</Link></li>
-              </ul>
-              <Link className='get_direction' to="" target="_blank"><FaPaperPlane /> Get Direction</Link>
-           </div>
-        </div>
-        <div className='col-md-1'> </div>
+            <div className="col-md-5 position-relative">
+              <div className="head_location">
+                <img src={REGISTERED_ICON} />
+                <h2>Registered Office</h2>
+                <ul>
+                  <li className="address">
+                    Level 7, Mfar Greenheart, Manyata Tech Park, Phase IV,
+                    Hebbal ORR, Bengaluru – 560045.
+                  </li>
+                  <li className="email">
+                    <Link to="mailto:info@sakshath-technologies.com">
+                      info@sakshath-technologies.com
+                    </Link>
+                  </li>
+                  <li className="phn">
+                    <Link to="tel:+918067819878">+91 8067819878</Link>
+                  </li>
+                </ul>
+                <Link
+                  className="get_direction"
+                  to="https://maps.app.goo.gl/MP3dg1KJYNCWxZJv5"
+                  target="_blank"
+                >
+                  <FaPaperPlane /> Get Direction
+                </Link>
+              </div>
+            </div>
+            <div className="col-md-1"> </div>
+          </div>
+
+          <div className="contact_page_social_icon">
+        <Link to="https://www.instagram.com/sakshath_technologies/" target="_blank">
+          <img src={INSTAGRAM_ICON} />
+        </Link>
+        <Link to="https://www.linkedin.com/company/sakshath-technologies/" target="_blank">
+          <img src={LINKEDIN_ICON} />
+        </Link>
+        <Link to="https://www.youtube.com/watch?v=UHjZFuO8AYY" target="_blank">
+          <img src={YOUTUBE_ICON} />
+        </Link>
       </div>
 
-<div className='row justify-content-center contact_social_icon'>
-<img src={INSTAGRAM_ICON} /> <img src={LINKEDIN_ICON} />
-</div>
-
-    </div>
-</section>
 
 
+        </div>
+      </section>
 
-{/* <section> 
+ 
+      {/* <section> 
   <div className='container'>
     <div className='row'>
 
     </div>
   </div>
 </section> */}
-<section className='our_location_sec'>
-  <div className="container">
+      <section className="our_location_sec">
+        <div className="container">
+          <div className="row">
+            <div className="text-center">
+              <Heading heading="Our Location" uppercase="text-uppercase" />
+            </div>
+          </div>
 
-<div className='row'>
-  <div className='text-center'>
-  <Heading heading="Our Location" uppercase="text-uppercase" />
-  </div>
-</div>
+          <div className="row our_location_tab">
+            <div className="col-md-12">
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+              >
+                {/* Asia Start */}
+                <Tab eventKey="home" title="Asia">
+                  <Tab.Container
+                    id="left-tabs-example"
+                    defaultActiveKey="bangalore"
+                  >
+                    <Row className="align-items-center map_sec">
+                      <Col sm={2}>
+                        <Nav variant="pills" className="flex-column">
+                          <Nav.Item>
+                            <Nav.Link eventKey="bangalore">Bangalore</Nav.Link>
+                          </Nav.Item>
+                          <Nav.Item>
+                            <Nav.Link eventKey="mumbai">Mumbai</Nav.Link>
+                          </Nav.Item>
+                          <Nav.Item>
+                            <Nav.Link eventKey="gurgram">Gurugram</Nav.Link>
+                          </Nav.Item>
+                        </Nav>
+                      </Col>
+                      <Col sm={10}>
+                        <Tab.Content>
+                          <Tab.Pane eventKey="bangalore">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.847152799348!2d77.6212673!3d13.045399399999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae176fbb02b8e9%3A0xefe5b09d3734be86!2sThe%20Executive%20Centre%20-%20Mfar%20Greenheart%2C%20Manyata%20Tech%20Park%20%7C%20Coworking%20Space%2C%20Serviced%20%26%20Virtual%20Offices%20and%20Workspace!5e0!3m2!1sen!2sin!4v1708024520218!5m2!1sen!2sin" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                          </Tab.Pane>
+                          <Tab.Pane eventKey="mumbai">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.847152799348!2d77.6212673!3d13.045399399999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae176fbb02b8e9%3A0xefe5b09d3734be86!2sThe%20Executive%20Centre%20-%20Mfar%20Greenheart%2C%20Manyata%20Tech%20Park%20%7C%20Coworking%20Space%2C%20Serviced%20%26%20Virtual%20Offices%20and%20Workspace!5e0!3m2!1sen!2sin!4v1708024520218!5m2!1sen!2sin" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                          </Tab.Pane>
+                          <Tab.Pane eventKey="gurgram">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.847152799348!2d77.6212673!3d13.045399399999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae176fbb02b8e9%3A0xefe5b09d3734be86!2sThe%20Executive%20Centre%20-%20Mfar%20Greenheart%2C%20Manyata%20Tech%20Park%20%7C%20Coworking%20Space%2C%20Serviced%20%26%20Virtual%20Offices%20and%20Workspace!5e0!3m2!1sen!2sin!4v1708024520218!5m2!1sen!2sin" width="100%" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                          </Tab.Pane>
+                        </Tab.Content>
+                      </Col>
+                    </Row>
+                  </Tab.Container>
+                </Tab>
+                {/* Asia End */}
 
-    <div className="row our_location_tab">
-      <div className='col-md-12'>
-      <Tabs
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="mb-3"
-      >
-      {/* Asia Start */}
-      <Tab eventKey="home" title="Asia">
-          <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-          <Row>
-            <Col sm={3}>
-              <Nav variant="pills" className="flex-column">
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            <Col sm={9}>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">First tab content</Tab.Pane>
-                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
-      </Tab>
-      {/* Asia End */}
-      
-      <Tab eventKey="us" title="US">
-        US
-      </Tab>
-      <Tab eventKey="anz" title="ANZ">
-        ANZ
-      </Tab>
-      <Tab eventKey="aftrica" title="Africa">
-        Africa
-      </Tab>
-      <Tab eventKey="middleeast" title="Middle East">
-        Middle East
-      </Tab>
-      <Tab eventKey="euorpe" title="Europe">
-        Europe
-      </Tab>
-    </Tabs>
-      </div>
-    </div>
-  </div>
-</section>
+                <Tab eventKey="us" title="US">
+                  US
+                </Tab>
+                <Tab eventKey="anz" title="ANZ">
+                  ANZ
+                </Tab>
+                <Tab eventKey="aftrica" title="Africa">
+                  Africa
+                </Tab>
+                <Tab eventKey="middleeast" title="Middle East">
+                  Middle East
+                </Tab>
+                <Tab eventKey="euorpe" title="Europe">
+                  Europe
+                </Tab>
+              </Tabs>
+            </div>
+          </div>
+        </div>
+      </section>
 
-        {/* <section  className='contact_us'>
+      {/* <section  className='contact_us'>
 
          <div className="container contact_us_box">
           <div className="row">
@@ -283,9 +337,8 @@ Gurugram, Haryana 122016
           </div>
         </div>
       </section> */}
-        
     </>
-  )
-}
+  );
+};
 
 export default Contactus;
