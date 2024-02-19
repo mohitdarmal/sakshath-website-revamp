b4a87d845aa059898517d5ccf92abfa5759cd01d
unknown <keeranaharidas12@gmail.com>
2024-02-19 11:02:27 +0530

    working on sticky service menu

--- src/Components/ServiceStickMenu/index.js
+++ src/Components/ServiceStickMenu/index.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./styles.scss";
 import { NavLink } from "react-router-dom";
 import { HiOutlineGlobeAlt } from "react-icons/hi";
 import SlideAnimationButton from "../../Components/Utils/SlideAnimationButton";
 import Heading from "../Utils/Heading";
 
-const ServiceStickMenu = (props) => {
-  return (
-    <>
-      {/* <div className="sticky_menu sticky-top">
+
+const ServiceStickMenu=(props)=>
+{
+    return(
+        <>
+{/* <div className="sticky_menu sticky-top">
 <div className="container-fluid"> */}
-      {/* <Navbar collapseOnSelect expand="lg" className="service_sticky_menu"  sticky="top" > */}
-      {/* <Navbar collapseOnSelect expand="lg" className="service_sticky_menu  sticky-top " >
+{/* <Navbar collapseOnSelect expand="lg" className="service_sticky_menu"  sticky="top" > */}
+{/* <Navbar collapseOnSelect expand="lg" className="service_sticky_menu  sticky-top " >
 <Container fluid className="service_sticky_menu_container">
   <Navbar.Brand href="#home" className="integration_heading">Integration </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -35,84 +37,56 @@ const ServiceStickMenu = (props) => {
 </div>
 </div> */}
 
-      {/* Main Header starts */}
-      <section className="sticky_menu sticky-top">
-        <div className="">
-          <nav
-            className={`navbar navbar-expand-md bsb-navbar bsb-navbar-hover bsb-navbar-caret stick_menu_bar`}
+
+
+{/* Main Header starts */}
+<section className="sticky_menu sticky-top">
+<div className="">
+<nav
+        className={`navbar navbar-expand-md bsb-navbar bsb-navbar-hover bsb-navbar-caret stick_menu_bar`}
+      >
+        <div className="container-fluid" id="service_stickmenu_list">
+          <h5 className="integration_heading"> Integration</h5>
+          <button
+            className="navbar-toggler border-0"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
           >
-            <div className="container-fluid" id="service_stickmenu_list">
-              <h5 className="integration_heading"> {props.pageName}</h5>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              className="bi bi-list"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </button>
+          <div
+            className="offcanvas offcanvas-end"
+            tabIndex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                Menu
+              </h5>
               <button
-                className="navbar-toggler border-0"
                 type="button"
-                data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="currentColor"
-                  className="bi bi-list"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                  />
-                </svg>
-              </button>
-              <div
-                className="offcanvas offcanvas-end"
-                tabIndex="-1"
-                id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel"
-              >
-                <div className="offcanvas-header">
-                  <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                    Menu
-                  </h5>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    data-bs-dismiss="offcanvas"
-                    aria-label="Close"
-                  ></button>
-                </div>
-                <div className="offcanvas-body header_menu">
-                  <ul className="navbar-nav justify-content-center flex-grow-1 service_stickmenu_list">
-                    <li>
-                      <NavLink to="#" className="nav-link ">
-                        {props.title1}
-                      </NavLink>
-                    </li>
-
-                    <li>
-                      <NavLink to="#" className="nav-link ">
-                        {props.title2}
-                      </NavLink>
-                    </li>
-
-                    <li>
-                      <NavLink to="#" className="nav-link ">
-                        {props.title3}
-                      </NavLink>
-                    </li>
-
-                    <li>
-                      <NavLink to="#" className="nav-link ">
-                       {props.title4}
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
             </div>
-<<<<<<< HEAD
             <div className="offcanvas-body header_menu">
-              <ul className="navbar-nav justify-content-center flex-grow-1 service_stickmenu_list">
+              <ul className="navbar-nav justify-content-end flex-grow-1 service_stickmenu_list">
            
               
                 
@@ -136,7 +110,6 @@ const ServiceStickMenu = (props) => {
                   <p data-to-scrollspy-id="third" className="paragraph">  Migration Technologies</p>
                   </NavLink>
                 </li>
-
                 <li>
                   <NavLink to="" className="nav-link ">
                   <p data-to-scrollspy-id="four" className="paragraph">  Managed Services</p>
@@ -151,12 +124,11 @@ const ServiceStickMenu = (props) => {
                
             </div>
           </div>
-=======
-          </nav>
->>>>>>> a8c507e1cf7519928564fa65271df17c557a8f72
         </div>
+      </nav>
+      </div>
       </section>
-    </>
-  );
+</>
+    )
 };
 export default ServiceStickMenu;
