9d29290186c3bd33ca60540ef4cee7377192bcc6
unknown <as4044579@gmail.com>
2024-02-23 18:35:40 +0530

    Staffing working in process

--- src/Pages/Staffing/Constatnt.js
+++ src/Pages/Staffing/Constatnt.js
@@ -1,5 +1,11 @@
 
 // export const ABOUT_WHO_WE_ARE=require('../../../Assets/images/who_we_r_img.png');
 
-export const ABOUT_WHO_WE_ARE=require('../../Assets/images/who_we_r_img.png');
+export const ABOUT_WHO_WE_ARE=require('../../Assets/images/Staffing_right_img.svg').default;
 export const RPA_BOT = require("../../Assets/images/rpa_bot_img3.png");
+
+
+export const STAFFING_BENIFIT=require('../../Assets/images/Staffing_benifit_img_1.png')
+export const STAFFING_ASSESSMENT=require('../../Assets/images/Assessment_staffing.png')
+ export const STAFFING_DATABASE=require('../../Assets/images/database-server.png');
+// export const STAFFING_DATABASE=require('../../Assets/images/database.png');
--- src/Pages/Staffing/index.js
+++ src/Pages/Staffing/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./style.scss";
-import { ABOUT_WHO_WE_ARE, RPA_BOT } from "./Constatnt";
+import {
+  ABOUT_WHO_WE_ARE,
+  RPA_BOT,
+  STAFFING_ASSESSMENT,
+  STAFFING_BENIFIT,
+  STAFFING_DATABASE,
+} from "./Constatnt";
 import Breadcumb from "../../Components/Breadcumb";
 import { APPLICATION_DEVLOPMENT_BREADCUMB_IMG } from "../Service/ApplicationDevelopment/Constant";
 import Heading from "../../Components/Utils/Heading";
 import CallToAction from "../../Components/CallToAction";
+import { Fade } from "react-awesome-reveal";
+import SlideAnimationButton from "../../Components/Utils/SlideAnimationButton";
 const Staffing = () => {
   return (
     <>
@@ -17,10 +25,12 @@ const Staffing = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
+            <Fade direction="up" delay={300} triggerOnce>
               <div className="staffing_main_headig">
                 <Heading heading="Helping Organizations Match Great Talent to Great Opportunity" />
               </div>
-
+              </Fade>
+              <Fade direction="up" delay={300} triggerOnce>
               <div className="staffing_content">
                 {/* <ul> */}
                 <li>
@@ -42,9 +52,16 @@ const Staffing = () => {
                   on improving productivity, reducing costs, ensuring statutory
                   compliance, and facilitating swift mobilization and
                   demobilization of resources.
+                  <br />
+                  <br />
+
+
+
+                  <SlideAnimationButton btnName="Get Started Now" url="/contact-us" />
                 </li>
                 {/* </ul> */}
               </div>
+              </Fade>
             </div>
             <div className="col-md-6">
               <div className="staffing_img">
@@ -57,6 +74,9 @@ const Staffing = () => {
 
       {/* staffing mian ends */}
 
+
+     
+
       {/* staffing benfits start*/}
       <section className="staffing_industry_offer_section">
         <div className="container">
@@ -67,50 +87,56 @@ const Staffing = () => {
           </div>
 
           <div className="row">
+        
             <div className="col-md-3">
+            <Fade direction="up"  triggerOnce>
               <div className="staffing_industry_offer_box ">
                 <img src={RPA_BOT} alt="" />
                 <h5>Fintech</h5>
               </div>
+              </Fade>
             </div>
             <div className="col-md-3">
+            <Fade direction="up"  triggerOnce>
               <div className="staffing_industry_offer_box ">
                 <img src={RPA_BOT} alt="" />
                 <h5>IT & ITES</h5>
               </div>
+              </Fade>
             </div>
             <div className="col-md-3">
+            <Fade direction="up"  triggerOnce>
               <div className="staffing_industry_offer_box ">
                 <img src={RPA_BOT} alt="" />
                 <h5>Telecom</h5>
               </div>
+              </Fade>
             </div>
             <div className="col-md-3">
+            <Fade direction="up"  triggerOnce>
               <div className="staffing_industry_offer_box ">
                 <img src={RPA_BOT} alt="" />
                 <h5>Automotive</h5>
               </div>
+              </Fade>
             </div>
+          
           </div>
+        
         </div>
       </section>
       {/* staffing benfits end */}
 
-      {/* Staffing services we offer starts*/}
-      <section>
-        <div className="container">
-          <div className="row"></div>
-        </div>
-      </section>
-      {/* Staffing services we offer ends */}
-
-      {/* content2 */}
+ {/* content2 */}
+ <Fade direction="up"  triggerOnce>
       <CallToAction
         ctaContent={
           "We focus on providing a quality staffing service for our clients and a smooth career transition for our candidates. "
         }
       />
+      </Fade>
       {/* content2 */}
+      
 
       <section className="staffing_why_sakshath">
         <div className="container">
@@ -159,59 +185,66 @@ const Staffing = () => {
         </div>
       </section>
 
-
-      <section>
+      <section className="our_staf_service_section">
         <div className="container">
           <div className="row">
             <div className="staffing_service text-center">
-              <Heading heading="Our Staffing Services"/>
+              <Heading heading="Our Staffing Services" />
 
               <p>
-              Our primary objective is to provide a quality staffing service to our clients and to help our candidates transition smoothly from one career to another. As a result, we have created the perfect synergy between technology and human interaction, which enables us to achieve measurable and effective results.
+                Our primary objective is to provide a quality staffing service
+                to our clients and to help our candidates transition smoothly
+                from one career to another. As a result, we have created the
+                perfect synergy between technology and human interaction, which
+                enables us to achieve measurable and effective results.
               </p>
             </div>
           </div>
         </div>
       </section>
 
-
-      <section>
+      <section className="staffing_job_section">
         <div className="container">
           <div className="row">
             <div className="col-md-4">
               <div className="staffing_job_box">
-              <h4>
-              Contract
-
-              </h4>
-              <p>
-              Placements on a contract basis are ideal for companies that manage multiple projects and require employees to fill multiple or various roles and positions. If you need assistance finishing an ongoing project or taking one from start to finish, we have expert consultants who are ready to get started immediately and deliver the results that you need.
-              </p>
-
+                <h4>Contract</h4>
+                <p>
+                  Placements on a contract basis are ideal for companies that
+                  manage multiple projects and require employees to fill
+                  multiple or various roles and positions. If you need
+                  assistance finishing an ongoing project or taking one from
+                  start to finish, we have expert consultants who are ready to
+                  get started immediately and deliver the results that you need.
+                </p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="staffing_job_box">
-              <h4>
-              Contract
-
-              </h4>
-              <p>
-              Placements on a contract basis are ideal for companies that manage multiple projects and require employees to fill multiple or various roles and positions. If you need assistance finishing an ongoing project or taking one from start to finish, we have expert consultants who are ready to get started immediately and deliver the results that you need.
-              </p>
-
+                <h4>Contract-to-Hire</h4>
+                <p>
+                  In contrast to contract placements, contract-to-hire
+                  placements are similar to contract placements, with the
+                  exception that the company usually intends to hire the
+                  consultant on a Full-Time basis at some point in the future.
+                  The company has the opportunity to evaluate the performance of
+                  the contractor before making a formal offer of employment
+                  under a contract-to-hire arrangement. Before moving on to a
+                  full-time position, the consultant will be employed as a
+                  contractor for a designated evaluation period.
+                </p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="staffing_job_box">
-              <h4>
-              Contract
-
-              </h4>
-              <p>
-              Placements on a contract basis are ideal for companies that manage multiple projects and require employees to fill multiple or various roles and positions. If you need assistance finishing an ongoing project or taking one from start to finish, we have expert consultants who are ready to get started immediately and deliver the results that you need.
-              </p>
-
+                <h4>Full Time</h4>
+                <p>
+                  Full-time employment is the perfect option for companies that
+                  seek long-term resources and solutions. At Sakshath Staffing,
+                  we specialize in establishing valuable connections with top
+                  permanent placement services that give companies the ability
+                  to hire permanent employees immediately.
+                </p>
               </div>
             </div>
             {/* <div className="col-md-4"></div> */}
@@ -220,8 +253,99 @@ const Staffing = () => {
         </div>
       </section>
 
+      {/* Staffing Benifits starts */}
+      <section className="staffing_benifit_section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center benifit_heading">
+              <Heading heading="Benefits" />
+            </div>
+          </div>
+          <div className="row">
+            {/* <div className="col-md-12"> */}
+            <div className="col-md-2"></div>
+            <div className="col-md-8 d-flex">
+              <div className="col-md-3 benifits_col_box_1">
+                <div className="staffing_benifit">
+                  <h6>Effective and Unconventional Recruitment Strategies</h6>
+                  <img src={STAFFING_BENIFIT} alt="" />
+                </div>
+              </div>
+              <div className="col-md-3 benifits_col_box_2">
+                <div className="staffing_benifit">
+                  <h6>Pre- Qualification Assessment</h6>
+                  <img src={STAFFING_ASSESSMENT} alt="" />
+                </div>
+              </div>
+              <div className="col-md-3 benifits_col_box_1">
+                <div className="staffing_benifit ">
+                  <h6> Strong Database</h6>
+                  <img src={STAFFING_DATABASE} alt="" />
+                </div>
+              </div>
+              <div className="col-md-3 benifits_col_box_2">
+                <div className="staffing_benifit">
+                  <h6> SLA Driven Delivery Approach</h6>
+                  <img src={STAFFING_BENIFIT} alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2"></div>
+            {/* </div> */}
+          </div>
+        </div>
+      </section>
+      {/* Staffing Benifits ends */}
 
-      {/*  */}
+      {/* Last three dicovery starts */}
+
+      <section className="discovery_section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 ">
+              <div className="box_dis">
+                <div className="dicovery_box">
+                  <h4>Discovery</h4>
+
+                  <p>
+                    We dig in with client leadership to understand the current
+                    situation, desired future state, and team culture to develop
+                    a clear picture of the ideal candidate to effect positive
+                    change within the organization.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 ">
+              <div className="box_dis">
+                <div className="dicovery_box">
+                  <h4>Analyse</h4>
+
+                  <p>
+                    Building on Discovery, we research to find the right
+                    framework to identify and target the candidate profiles
+                    needed to build client teams that exceed expectations.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="box_dis">
+                <div className="dicovery_box">
+                  <h4>Execute</h4>
+
+                  <p>
+                    Leveraging our network and extensive experience, augmented
+                    by proprietary technology, we deliver and iterate based on
+                    client feedback to ensure successful selection.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* discovery end */}
     </>
   );
 };
