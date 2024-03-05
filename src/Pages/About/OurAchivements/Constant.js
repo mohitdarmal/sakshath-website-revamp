bc02fc099790e5dbc52316d2c940fd93bb7cb9cc
unknown <keeranaharidas12@gmail.com>
2024-03-05 13:13:21 +0530

    achievement

--- src/Pages/About/OurAchivements/Constant.js
+++ src/Pages/About/OurAchivements/Constant.js
@@ -1,3 +1,3 @@
  export const ACHIVMENTS_ONE=require("../../../Assets/images/Achivement40.png");
- export const ACHIVMENTS_TWO=require("../../../Assets/images/achievement_img_two.jpeg");
+ export const ACHIVMENTS_TWO=require("../../../Assets/images/achivement_stall.png");
  export const ACHIVMENTS_THREE=require("../../../Assets/images/achievement_img_three.jpg");
