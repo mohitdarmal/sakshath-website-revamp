2a9e0729a7434a709a4c5ae2c19eb0ed88d476ec
SakshathGit <mohit.darmal@sakshath-technologies.com>
2024-06-05 15:34:30 +0530

    changes in about us environment and .env file

---
+++ src/environment.js
@@ -0,0 +1,18 @@
+const PROD_BASE_URL = process.env.REACT_BASE_URL;
+
+const prod = {
+MOBILE-OTP: `${PROD_BASE_URL}/MOBILE/OTP`
+}
+
+const PREpROD = {
+    MOBILE-OTP: `${PROD_BASE_URL}/MOBILE/OTP`
+    }
+
+    const local = {
+        MOBILE-OTP: `${PROD_BASE_URL}/MOBILE/OTP`
+        }
+
+
+        const environment = prod;
+
+        export default environment;
